4706b4aba6675334ee3e7d1b86765122049e16f4
lluiset <eseramon@gmail.com>
2021-06-01 19:53:27 +0200

    Email confirmacio referencia modificat amb HTML

--- limbo/src/es/cc/esliceu/db/limbo/controller/PantallaRegistreController.java
+++ limbo/src/es/cc/esliceu/db/limbo/controller/PantallaRegistreController.java
@@ -36,7 +36,7 @@ public class PantallaRegistreController {
     }
 
     public void sendEmail(String email, String referenciaGenerada) {
-        EnviadorEmail.enviaEmail(email, "Codi de Referencia", " Aquí tens el codi de referencia: " + referenciaGenerada);
+        EnviadorEmail.enviaEmail(email, "Codi de Referencia", this.generaEmail(referenciaGenerada));
     }
 
     public boolean checkReference(String referencia, String referenciaGenerada) {
@@ -68,6 +68,33 @@ public class PantallaRegistreController {
     public void registerSuccessful() {
         PantallaInicialController.getInstance().init();
     }
+
+    private String generaEmail(String referenciaGenerada) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<body style=\"background-color: #2286c3\">");
+        sb.append("<div style='flex-basis: 100%;\n" +
+                "        display: flex;\n" +
+                "        justify-content: center;\n" +
+                "        align-content: center;\n" +
+                "        align-items: center;'>\n" +
+                "    <img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/LIMBO_Logo.png/1200px-LIMBO_Logo.png\" width=\"300\" height=\"100\" alt=\"SharkApk\"/>\n" +
+                "</div>");
+        sb.append("<div style=\"margin: 20px auto; border-radius: 20px; height: 100vh;background-color: #e0e0e0; width: 50%; display: flex; flex-direction: column; justify-content: flex-start; align-content: space-around;\">");
+        sb.append("<div style=\"display: flex; border-top-right-radius: 20px; border-top-left-radius: 20px; flex-direction: row; justify-content: center; background-color: #64b5f6\">");
+        sb.append("<h1>Codi de referència</h1>");
+        sb.append("</div>");
+        sb.append("<div style=\"padding:10px\">");
+        sb.append("<h2>Hola,</h2>");
+        sb.append("<p>Aquí tienes tu código de referència para confirmar el registro en Limbo: " + "<strong>" + referenciaGenerada + "</strong></p>");
+        sb.append("<p>Muchas gracias por seguir confiando en <strong>Limbo</strong></p>");
+        sb.append("<p>¡Hasta la próxima!</p>");
+        sb.append("</div>");
+        sb.append("</div>");
+        sb.append("</body>");
+        return sb.toString();
+    }
+
+
 
     public synchronized static PantallaRegistreController getInstance() {
         if (instance == null) {
